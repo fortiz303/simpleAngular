d8815183d020bab3d0969aff069ef40c48a8131a
admin <admin@PATC21.local>
2019-05-14 12:23:19 -0700

    simple angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title = 'Tour Of Heroes';
+  twoTitle = 'this is the variable twoTitle';
+ 
 }
